c408dec35fc776a6eb063e6a0480dda6296e42ba
DAM <aterik.dev@gmail.com>
2024-09-23 11:01:49 +0200

    Nombre de Aplicacion

--- src/main/java/com/erikat/ej_repaso_fx/InitApp.java
+++ src/main/java/com/erikat/ej_repaso_fx/InitApp.java
@@ -12,7 +12,7 @@ public class InitApp extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(InitApp.class.getResource("MainView.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Gestor de Clientes");
         stage.setScene(scene);
         stage.show();
     }
